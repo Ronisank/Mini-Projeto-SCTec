d759301299bbc3ae45572ce81a5c14952981ce9b
Roni <ronisank21@hotmail.com>
2026-08-02 18:22:02 -0300

    criado-a-classe-livro

---
+++ livro.js
@@ -0,0 +1,9 @@
+export class Livro {
+    constructor(titulo, autor, categoria, paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.paginas = paginas;
+        this.disponivel = true;
+    }
+}
